afad44645427a663e79cf783b2ed1fa494b6ef99
arsene1989 <89330664@qq.com>
2017-12-02 10:00:01 +0800

    modal

--- wechat-weapp-108mall-master/pages/money/index.js
+++ wechat-weapp-108mall-master/pages/money/index.js
@@ -7,8 +7,8 @@ Page({
    */
   data: {
     userInfo: {},
-    money:0
-  
+    money:0,
+    showModalStatus: false
   },
 
   /**
@@ -87,4 +87,79 @@ Page({
       url: '../details/index',
     })
   },
+
+  withdraw: function () {
+    wx.showModal({
+      title: '提现',
+      content: '请您输入提现金额',
+    })
+  },
+
+  recharge: function () {
+    wx.showModal({
+      title: '充值',
+      content: '请您输入充值金额',
+    })
+  },
+  powerDrawer: function (e) {
+    var currentStatu = e.currentTarget.dataset.statu;
+    console.log(currentStatu);
+    this.util(currentStatu)
+  },
+
+  binphone: function (e) {
+    console.log(e.detail.value)
+
+  },
+
+  binpassword: function (e) {
+    console.log(e.detail.value)
+  },
+
+  util: function (currentStatu) {
+    /* 动画部分 */
+    // 第1步：创建动画实例   
+    var animation = wx.createAnimation({
+      duration: 200,  //动画时长  
+      timingFunction: "linear", //线性  
+      delay: 0  //0则不延迟  
+    });
+    // 第2步：这个动画实例赋给当前的动画实例  
+    this.animation = animation;
+    // 第3步：执行第一组动画  
+    animation.opacity(0).rotateX(-100).step();
+    // 第4步：导出动画对象赋给数据对象储存  
+    this.setData({
+      animationData: animation.export()
+    })
+    // 第5步：设置定时器到指定时候后，执行第二组动画  
+    setTimeout(function () {
+      // 执行第二组动画  
+      animation.opacity(1).rotateX(0).step();
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象  
+      this.setData({
+        animationData: animation
+      })
+
+      //关闭  
+      if (currentStatu == "close") {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
+    }.bind(this), 200)
+
+    // 显示  
+    if (currentStatu == "open") {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
+    }
+  }
+
+  
 })
