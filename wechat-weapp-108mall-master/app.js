4919bfa14f5537e06baa17bc8a4fd0ad33ac79a5
arsene1989 <89330664@qq.com>
2017-12-10 16:44:15 +0800

    appid

--- wechat-weapp-108mall-master/app.js
+++ wechat-weapp-108mall-master/app.js
@@ -1,12 +1,15 @@
 //app.js
 App({
 
+  
+  
   onLaunch: function () {
     console.log('App Launch')
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    wx.setStorageSync('logs', logs) 
+    
   },
   getUserInfo:function(cb){
     var that = this
@@ -20,6 +23,38 @@ App({
             success: function (res) {
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
+              console.log('res',res)
+              if(res.code) {
+                wx.request({
+                  //获取openid接口  
+                  url: 'https://api.weixin.qq.com/sns/jscode2session',
+                  data: {
+                    appid: 'wxc8f32ff46c0f44a7',
+                    secret: '1bf6489da0fae7f171c824e21f04bb99',
+                    js_code: res.code,
+                    grant_type: 'authorization_code'
+                  },
+                  method: 'GET',
+                  success: function (res) {
+                    console.log(res.data)
+                    OPEN_ID = res.data.openid;//获取到的openid  
+                    SESSION_KEY = res.data.session_key;//获取到session_key
+                    wx.showToast({
+                      title: 'open_id', OPEN_ID,
+                    })
+                    console.log(OPEN_ID.length)
+                    console.log(SESSION_KEY.length)
+                    that.setData({
+                      open_id: res.data.openid,
+                      session_key: res.data.session_key
+                    })
+                  }
+                })
+              } else {
+                wx.showToast({
+                  title: 'code不存在',
+                })
+              }
             }
           })
         }
@@ -32,6 +67,9 @@ App({
   onHide: function () {
     console.log('App Hide')
   },
+
+  
+
   globalData:{
     userInfo:null,
     coin_price:1,
@@ -44,6 +82,8 @@ App({
     cert: "",
     bank_account: "",
     bank_open:"",
-    money:95.27
+    money:95.27,
+    open_id:"",
+    session_key:""
   }
 })
